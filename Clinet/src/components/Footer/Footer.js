bb45981bb3a1b82fef234bfd302ce07441cb84e0
Msotafa Mohamed <mosatfa346@gmail.com>
2022-01-22 09:04:44 +0200

    footer-components

---
+++ Clinet/src/components/Footer/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import "../../css/Footer/footer.css"
+export const Footer = () => {
+  return <footer>
+      <h6> &copy; Copyright Mostafa Mohamed 2022</h6>
+  </footer>;
+};
