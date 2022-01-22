22945b559c8770a7cfacd90dbe4cf7b4bfd65c82
Msotafa Mohamed <mosatfa346@gmail.com>
2022-01-22 11:23:37 +0200

    product-compnents

--- Clinet/src/App.js
+++ Clinet/src/App.js
@@ -1,16 +1,31 @@
-import React from 'react';
-import { Footer } from './components/Footer/Footer';
-import Header from './components/Header/Header';
-
+import React, { useState } from "react";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Products from "./components/Products/Products";
+import data from "./data/data.json";
 const App = () => {
-  return <main className="layout">
-      <Header/>
+  const [product] = useState(data);
+  return (
+    <main className="layout">
+      <Header />
       <div className="content">
-
-        Content
+        <div className="wrapper">
+          <div className="product-container">
+            {product.map(({ id, title, desc, img, price }) => (
+              <Products
+                key={id}
+                title={title}
+                img={img}
+                price={price}
+                desc={desc}
+              />
+            ))}
+          </div>
+        </div>
       </div>
-      <Footer/>
-  </main>;
+      <Footer />
+    </main>
+  );
 };
 
 export default App;
